17b552706d825a655c1495ed88ba8bd4bb004b4a
NIKHILESH KUMAR REDDY ANAM <nikhilesh@NIKHILESHs-MacBook-Air.local>
2023-06-26 18:54:53 +0530

    parallel sum

--- go_primer/parallel_sum.go
+++ go_primer/parallel_sum.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"io"
+	"os"
 	"strconv"
 )
 
@@ -12,6 +13,14 @@ import (
 func sumWorker(nums chan int, out chan int) {
 	// TODO: implement me
 	// HINT: use for loop over `nums`
+	var sum int
+
+	for num := range nums {
+		sum = sum + num
+	}
+
+	out <- sum
+	close(out)
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -23,7 +32,36 @@ func sum(num int, fileName string) int {
 	// TODO: implement me
 	// HINT: use `readInts` and `sumWorkers`
 	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	file, err := os.Open(fileName)
+	checkError(err)
+	defer file.Close()
+
+	nums, err := readInts(file)
+	checkError(err)
+
+	numChunks := len(nums) / num
+	var outChannels []chan int
+
+	for chunk := 0; chunk < num; chunk++ {
+		channel := make(chan int, numChunks)
+		out := make(chan int, num)
+		outChannels = append(outChannels, out)
+		lowerBound := chunk * numChunks
+		upperBound := (chunk + 1) * numChunks
+		for _, numVal := range nums[lowerBound:upperBound] {
+			channel <- numVal
+		}
+		close(channel)
+		go sumWorker(channel, out)
+	}
+
+	result := 0
+	for _, channel := range outChannels {
+		partialSum := <-channel
+		result = result + partialSum
+	}
+
+	return result
 }
 
 // Read a list of integers separated by whitespace from `r`.
